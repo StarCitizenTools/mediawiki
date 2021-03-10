4f374ca628c062167954d4957060ffaea0a17db9
alistair3149 <alistair31494322@gmail.com>
2021-03-10 15:52:47 -0500

    Remove unused local settings

--- LocalSettings.php
+++ LocalSettings.php
@@ -7,10 +7,6 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 /* DEBUG ONLY */
 $wgShowExceptionDetails = true;
 
-#Tidy HTML output
-$wgUseTidy = true;
-$wgTidyConfig = [ 'driver' => 'RemexHtml' ];
-
 #General Settings
 $wgSitename = "Star Citizen Wiki";
 $wgMetaNamespace = "Star_Citizen";
@@ -44,10 +40,10 @@ $wgResourceBasePath = $wgScriptPath;
 
 ## The URL path to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
-$wgLogo = "$wgResourceBasePath/resources/assets/sitelogo.png";
-$wgLogoHD = [
-  "svg" => "$wgResourceBasePath/resources/assets/sitelogo.svg"
+$wgLogos = [
+	'svg' => "$wgResourceBasePath/resources/assets/sitelogo.svg",
 ];
+
 $wgFavicon = "$wgResourceBasePath/resources/assets/favicon.ico";
 $wgAppleTouchIcon = "$wgResourceBasePath/resources/assets/apple-touch-icon.png";
 
@@ -68,9 +64,6 @@ $wgHiddenPrefs[] = 'realname';
 # MySQL table options to use during installation or update
 $wgDBTableOptions = "ENGINE=InnoDB, DEFAULT CHARSET=utf8";
 
-# Experimental charset support for MySQL 5.0.
-$wgDBmysql5 = false;
-
 ## Shared memory settings
 $wgMainCacheType = 'redis';
 $wgSessionCacheType = 'redis';
@@ -85,40 +78,15 @@ $wgUseImageMagick = true;
 $wgThumbnailEpoch = "20190815000000";
 $wgIgnoreImageErrors = true;
 
-$wgDefaultUserOptions['imagesize'] = 4; // image size 1280, 1024
-
-$wgThumbLimits = array(
-  120, // thumb size 1
-  150, // thumb size 2
-  180, // thumb size 3
-  200, // thumb size 4
-  250, // thumb size 5
-  300 // thumb size 6
-);
-
-$wgDefaultUserOptions['thumbsize'] = 5; // thumb size 300
-
 $wgMaxImageArea = 6.4e7;
 
 # Gallery settings
 $wgGalleryOptions = [
-  'imagesPerRow' => 0, // Default number of images per-row in the gallery. 0: Adapt to screensize
-  'imageWidth' => 180, // Width of the cells containing images in galleries (in "px")
-  'imageHeight' => 180, // Height of the cells containing images in galleries (in "px")
-  'captionLength' => true, // Length of caption to truncate (in characters) in special pages or when the showfilename parameter is used
-                           // A value of 'true' will truncate the filename to one line using CSS.
-                           // Deprecated since 1.28. Default value of 25 before 1.28.
-  'showBytes' => true, // Show the filesize in bytes in categories
-    'mode' => 'packed', // One of "traditional", "nolines", "packed", "packed-hover", "packed-overlay", "slideshow" (1.28+)
+  'mode' => 'packed-overlay', // One of "traditional", "nolines", "packed", "packed-hover", "packed-overlay", "slideshow" (1.28+)
 ];
 
 # InstantCommons allows wiki to use images from https://commons.wikimedia.org
 $wgUseInstantCommons = true;
-
-## If you use ImageMagick (or any other shell command) on a
-## Linux server, this will need to be set to the name of an
-## available UTF-8 locale
-$wgShellLocale = "en_US.utf8";
 
 ## If you want to use image uploads under safe mode,
 ## create the directories images/archive, images/thumb and
@@ -141,14 +109,6 @@ $wgRightsPage = ""; # Set to the title of a wiki page that describes your licens
 $wgRightsUrl = "https://creativecommons.org/licenses/by-sa/4.0/";
 $wgRightsText = "Creative Commons Attribution-ShareAlike";
 $wgRightsIcon = "$wgResourceBasePath/resources/assets/licenses/cc-by-sa.png";
-
-# Enable new filters for RC
-# Depreciate in > MW 1.32
-$wgStructuredChangeFiltersShowPreference = true;
-$wgStructuredChangeFiltersOnWatchlist = true;
-
-# Path to the GNU diff3 utility. Used for conflict resolution.
-$wgDiff3 = "/usr/bin/diff3";
 
 # The following permissions were set based on your choice in the installer
 $wgAllowUserCss = true;
