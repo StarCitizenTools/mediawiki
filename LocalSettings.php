f96feb8743245c7c790cc14dd183218f7961425f
alistair3149 <alistair3149@users.noreply.github.com>
2021-03-18 17:16:55 -0400

    [Configuration] Revert wgRightsPage to blank

--- LocalSettings.php
+++ LocalSettings.php
@@ -130,7 +130,7 @@ $wgFooterLinkCacheExpiry = 31536000;
 ## For attaching licensing metadata to pages, and displaying an
 ## appropriate copyright notice / icon. GNU Free Documentation
 ## License and Creative Commons licenses are supported so far.
-$wgRightsPage = "MediaWiki:Copyright"; # Set to the title of a wiki page that describes your license/copyright
+$wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
 $wgRightsUrl = "https://creativecommons.org/licenses/by-sa/4.0/";
 $wgRightsText = "Creative Commons Attribution-ShareAlike";
 $wgRightsIcon = "$wgResourceBasePath/resources/assets/licenses/cc-by-sa.png";
