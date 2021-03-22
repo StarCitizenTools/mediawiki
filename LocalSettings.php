621668e0b14c13c54aa7e9b95220b335fdb254e2
alistair3149 <alistair31494322@gmail.com>
2021-03-22 11:13:21 -0400

    [Extension] Add CloudFlare

--- LocalSettings.php
+++ LocalSettings.php
@@ -202,6 +202,7 @@ wfLoadExtension( 'CirrusSearch' );
 wfLoadExtension( 'Cite' );
 wfLoadExtension( 'CiteThisPage' );
 wfLoadExtension( 'cldr' );
+wfLoadExtension( 'CloudFlare' );
 wfLoadExtension( 'CodeEditor' );
 wfLoadExtension( 'CodeMirror' );
 wfLoadExtension( 'CommonsMetadata' );
