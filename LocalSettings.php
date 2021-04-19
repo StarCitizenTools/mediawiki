bd1c681c471d0828a88ceccc30c929296b99530f
alistair3149 <alistair31494322@gmail.com>
2021-04-19 14:43:44 -0400

    Add Permissions Policy

--- LocalSettings.php
+++ LocalSettings.php
@@ -178,6 +178,9 @@ $wgCitizenEnableStrictReferrerPolicy = true;
 # Feature policy
 $wgCitizenEnableFeaturePolicy = true;
 $wgCitizenFeaturePolicyDirective = 'autoplay \'none\'; camera \'none\'; fullscreen \'self\'; geolocation \'none\'; microphone \'none\'; midi \'none\'; payment \'none\'' ;
+# Permissions policy
+$wgCitizenEnablePermissionsPolicy = true;
+$wgCitizenPermissionsPolicyDirective = 'accelerometer=(), autoplay=(), camera=(), geolocation=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=()' ;
 # Use REST API search endpoint
 $wgCitizenSearchUseREST = true;
 # Search description source
