4a77005d5570dc82de44b36e834e728826d38bf8
alistair3149 <alistair3149@users.noreply.github.com>
2021-08-10 23:30:41 -0400

    Disable jQuery Migrate

--- LocalSettings.php
+++ LocalSettings.php
@@ -39,6 +39,10 @@ $wgCdnServers[] = "api.fastly.com";
 ## Force HTTPS
 $wgForceHTTPS = true;
 
+# Don't include jQuery Migrate
+# TODO: Remember to remove it after 1.36+
+$wgIncludejQueryMigrate = false;
+
 ## Content Security Policy
 ## hCaptcha is required for VE
 ## Flickr API is required for UploadWizard
